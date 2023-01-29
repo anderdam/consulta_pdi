4e691fc74e2867c13c6b60ef9b0c41e7be5adff9
Anderson Damasceno <anderdam@gmail.com>
2023-01-29 19:25:48 -0300

    Pegar dados da tabela de faturas

--- consulta_pdi/__init__.py
+++ consulta_pdi/__init__.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 import time
 import pandas as pd
+import pygsheets
+
+# CONSTANTS
+TAG_NAME = 'tag name'
 
 # list of keys
 pdi_list = []
@@ -9,7 +13,9 @@ while not parar:
     pdi = int(input("Insira um PDI ou 0 para parar: "))
     if pdi == 0:
         parar = True
-    pdi_list.append(pdi)
+        pass
+    else:
+        pdi_list.append(pdi)
 
 key_list = pdi_list
 
@@ -21,37 +27,47 @@ url = 'https://agenciavirtual.sabesp.com.br/minhas-faturas'
 driver.get(url)
 
 # wait for the page to load
-time.sleep(5)
+time.sleep(2)
 
 # find the checkbox element and select it
-checkbox = driver.find_element(by='id', value="checkbox-id")
+checkbox = driver.find_element(by='id', value="_dxpminhasfaturas_WAR_dxpminhasfaturas_:minhasFaturasForm:chkSVR")
 checkbox.click()
+
+df = pd.DataFrame()
 
 # iterate through the list of keys
 for key in key_list:
     # find the input element and enter the key
-    input_element = driver.find_element(by='', value="_dxpminhasfaturas_WAR_dxpminhasfaturas_:minhasFaturasForm:chkSVR")
+    input_element = driver.find_element(by='id', value="_dxpminhasfaturas_WAR_dxpminhasfaturas_:minhasFaturasForm:pde")
     input_element.send_keys(key)
 
+    time.sleep(2)
     # wait for the user to submit the form manually
-    input("Press Enter to continue...")
+    # input("Press Enter to continue...")
 
     # find the table element
-    table = driver.find_element(by='class', value="criaTabelaDinamica")
-    # create an empty dataframe
-    df = pd.DataFrame()
+    table_div = driver.find_element(by='class name', value="criaTabelaDinamica")
+    childs_div = table_div.find_elements(by='xpath', value='./*')
+
+    for child in childs_div:
+        print(child.text)
     # add the 'Key' column to the dataframe
-    df['Key'] = key
-    # find the table headers
-    headers = [th.text for th in table.find_elements(by='tag_name', value="th")]
-    # iterate through each row
-    for i, row in enumerate(table.find_elements(by='tag_name', value="tr")):
-        # find all columns in the row
-        columns = row.find_elements(by='tag_name', value="td")
-        for j, column in enumerate(columns):
-            # add the data to the corresponding column
-            df.at[i, headers[j]] = column.text
-    print(df)
+#     df['Key'] = key
+#     # find the table headers
+#     headers = [th.text for th in table.find_elements(by=TAG_NAME, value="th")]
+#     # iterate through each row
+#     for i, row in enumerate(table.find_elements(by=TAG_NAME, value="tr")):
+#         # find all columns in the row
+#         columns = row.find_elements(by=TAG_NAME, value="td")
+#         for j, column in enumerate(columns):
+#             # add the data to the corresponding column
+#             df.at[i, headers[j]] = column.text
+#
+# print(df)
+# df.to_excel("/home/anderdam/Documents/dataframe.xlsx", engine='openpyxl')
 
 # close the browser
 #driver.close()
+
+
+# 0911757716
